28aee1440f2e907f2c50e75fc8f6965a22447472
PengWenJ008 <806340584@qq.com>
2022-10-26 21:47:30 +0800

    Modify main.c

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+int main() 
+{
+		double a = 1;
+	double b ;
+	double c = a - b;
+	double d = 0;
+	double e ;
+	double h = 0.001;
+	double m = 25;
+	printf("   x       y   \n");
+	while (c > 0.001*h&&e<m)
+	{
+	    b = a-a*h;
+	    c = a - b;
+		a = b;	
+		e = d + h;
+	    d = e;
+	    
+		printf("%lf   %lf\n", e, b);
+    }
+	
 	return 0;
 }
 
